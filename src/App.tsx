dd120c5fce481074fdea679297a724e1964748cc
Gleb Pilipenka <g.pilipenko@itransition.com>
2022-02-22 18:48:29 +0300

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import {Example_6} from 'examples/example_4_5_6/Example_6';
-import {SlowComponent} from 'examples/slowComponent/SlowComponent';
-import {Example_4} from 'examples/example_4_5_6/Example_4';
-import {Example_5} from 'examples/example_4_5_6/Example_5';
 import {Fragment} from 'react';
 
 export const App = () => {
@@ -10,13 +6,13 @@ export const App = () => {
       {/*<Example_1 />*/}
       {/*<Example_2 />*/}
       {/*<Example_3 />*/}
-      <Example_4 />
-      <hr />
-      <Example_5 />
-      <hr />
-      <Example_6>
-        <SlowComponent />
-      </Example_6>
+      {/*<Example_4 />*/}
+      {/*<hr />*/}
+      {/*<Example_5 />*/}
+      {/*<hr />*/}
+      {/*<Example_6>*/}
+      {/*  <SlowComponent />*/}
+      {/*</Example_6>*/}
       {/*<Example_7 />*/}
     </Fragment>
   );
